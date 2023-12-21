8fa7fe7d44643c5159db95459f043db6befdb75c
Ahmex2 <76560273+Ahmex2@users.noreply.github.com>
2023-12-21 12:01:42 +0200

    index.js

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// the authenication mechanism here
 if(req.session.authorization) { //get the authorization object stored in the session
     token = req.session.authorization['accessToken']; //retrieve the token from authorization object
-    jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
+    jwt.verify(token, "access",(err,user)=>{ 
         if(!err){
             req.user = user;
             next();
@@ -64,4 +64,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server IS RUNNING ON : http://localhost:5000 "));
